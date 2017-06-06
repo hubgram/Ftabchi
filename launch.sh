e277f8a45f2f56acdf156ea55d32d6eb746b59c9
Naji <teamnaji@gmail.com>
2017-06-07 00:52:45 +0430

    Just for Fun V2

--- launch.sh
+++ launch.sh
@@ -10,7 +10,7 @@ update() {
 
 install() {
 	sudo apt-get -y update && sudo apt-get -y upgrade 
-	sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat tmux libexpat1-dev
+	sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
 	sudo apt-get -y update && sudo apt-get -y upgrade 
 	git pull
 	git submodule update --init --recursive
@@ -31,22 +31,80 @@ install() {
 	cd ..
 }
 
-if [ "$1" = "install" ]; then
-	install
-elif [ "$1" = "update" ]; then
-	update
-else
-	if [ ! -f ./tg/telegram.h ]; then
-		echo "tg not found"
-		echo "Run $0 install"
-		exit 1
-	fi
+autolaunch() {
+	while true ; do
+		for oldtgbots in bot-*.lua ; do
+			Fbot="${oldtgbots%.*}"
+			num="${Fbot/bot-/}"
+			tmux kill-session -t Fbot-"$num"
+			for files in "$THIS_DIR"/.telegram-cli/Fbot-"$num"/downloads ; do
+				rm -rf $files/*
+			done
+			TMUX= tmux new-session -d -s Fbot-"$num" "bash launch.sh $num"
+			tmux detach -s Fbot-"$num"
+		done
+		echo -e " \n\e[1;32mربات ها راه اندازی شدند << \e[1;34m| Naji |\e[1;32m>> Bots are Running\n\e[0;39;49m"
+		sleep 1200
+	done 
+}
 
-	if [ ! -f ./tg/bin/telegram-cli ]; then
-		echo "tg binary not found"
-		echo "Run $0 install"
-		exit 1
+make_config() {
+	mkdir -p "$THIS_DIR"/.telegram-cli/Fbot"$1"
+	cfg="$THIS_DIR"/.telegram-cli/Fbot$1/Fbot.cfg
+	bot="$THIS_DIR"/bot-$1.lua
+	if [[ ! -f $cfg ]]; then
+		echo "default_profile = \"Fbot$1\";Fbot$1 = {config_directory = \"$THIS_DIR/.telegram-cli/Fbot$1\";test = false;msg_num = true;};" >> $cfg
 	fi
-	rm -r ../.telegram-cli/state
-	./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot.lua -l 1 -E $@
+	if [[ ! -f $bot ]]; then
+		cat bot.lua >> bot-$1.lua
+		sed -i 's/BOT-ID/'$1'/g' bot-$1.lua
+	fi
+}
+
+usage() {
+printf "\e[1;36m"
+  cat <<EOF
+
+>> Usage: $0 [options]
+    Options:
+	
+      install           Install the prerequisites of Bot
+      update            Update the source code of bot
+      help              Print this message for help
+      NUM               run bot by this NUM
+
+EOF
+
+printf "%s\n\e[0;39;49m"
+}
+
+if [ "$1" ]; then
+	if [ "$1" = "install" ]; then
+		install
+	elif [ "$1" = "update" ]; then
+		update
+	elif 
+		[ "$1" = "autolaunch" ]; then
+		autolaunch
+	else
+		if [[ "$1" =~ ^[0-9]+$ ]] ; then
+			if [ ! -f ./tg/telegram.h ]; then
+				echo "tg not found"
+				echo "Run $0 install"
+				exit 1
+			fi
+			if [ ! -f ./tg/bin/telegram-cli ]; then
+				echo "tg binary not found"
+				echo "Run $0 install"
+				exit 1
+			fi
+			make_config $1
+			rm -r /.telegram-cli/Fbot$1/state
+			./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot-$1.lua -E -c ./.telegram-cli/Fbot$1/Fbot.cfg "$@"
+		else
+			usage
+		fi
+	fi
+else
+	usage
 fi
